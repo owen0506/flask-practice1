1a115382f5ccf566785140ec8b3fa7ba0c1c372d
owen0506 <owen030506@gmail.com>
2026-09-22 12:32:32 +0900

    Add HW1 profile page

--- HW1/app.py
+++ HW1/app.py
@@ -2,6 +2,13 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def home():
     return render_template("index.html")
+
+
+@app.route("/profile")
+def profile():
+    hobbies = ["NBA 시청", "축구 보기", "여행 가기"]
+    return render_template("profile.html", hobbies=hobbies)
